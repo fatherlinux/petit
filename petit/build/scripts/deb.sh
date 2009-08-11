ee2d6dcafc35e5cb37f82f7b1b903f34c0bdf1e3
smccarty <smccarty@localhost>
2009-08-11 19:19:46 +0000

    * Patched Makefile to handle building of deb packages automagically SM

--- petit/build/scripts/deb.sh
+++ petit/build/scripts/deb.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-rm -rf deb/usr deb/var
+rm -rf deb/petit
 
 # Create directory structure
-mkdir -p deb/usr/bin
-mkdir -p deb/usr/share/doc/petit
-mkdir -p deb/var/lib/petit
+mkdir -p deb/petit/DEBIAN
+mkdir -p deb/petit/usr/bin
+mkdir -p deb/petit/usr/share/doc/petit
+mkdir -p deb/petit/var/lib/petit
+
+# Copy in control file
+cp deb/control deb/petit/DEBIAN/control
 
 # Now fill with latest data
 ## Bin
@@ -18,3 +22,9 @@ cp ../src/README deb/usr/share/doc/petit/README
 
 ## Lib
 rsync -av --exclude=.svn ../src/lib/ deb/var/lib/petit/
+
+# Build the package
+dpkg -b deb/petit petit_0.8.1_i386.deb
+
+# Clean up
+rm -rf deb/petit
