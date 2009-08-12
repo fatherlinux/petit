caab06e0ff391252a47640deb43c51120879debb
smccarty <smccarty@localhost>
2009-08-12 14:59:00 +0000

    * Updated to auto create version * patched file directories

--- petit/build/scripts/deb.sh
+++ petit/build/scripts/deb.sh
@@ -13,18 +13,19 @@ cp deb/control deb/petit/DEBIAN/control
 
 # Now fill with latest data
 ## Bin
-rsync -av --exclude=.svn ../src/bin/ deb/usr/bin/
+rsync -av --exclude=.svn ../src/bin/ deb/petit/usr/bin/
 
 ## Docs
-cp ../src/AUTHORS deb/usr/share/doc/petit/AUTHORS
-cp ../src/COPYING deb/usr/share/doc/petit/COPYING
-cp ../src/README deb/usr/share/doc/petit/README
+cp ../src/AUTHORS deb/petit/usr/share/doc/petit/AUTHORS
+cp ../src/COPYING deb/petit/usr/share/doc/petit/COPYING
+cp ../src/README deb/petit/usr/share/doc/petit/README
 
 ## Lib
-rsync -av --exclude=.svn ../src/lib/ deb/var/lib/petit/
+rsync -av --exclude=.svn ../src/lib/ deb/petit/var/lib/petit/
 
 # Build the package
-dpkg -b deb/petit petit_0.8.1_i386.deb
+version=`cat deb/control | grep Version | awk '{print $2}'`
+dpkg -b deb/petit petit_${version}_i386.deb
 
 # Clean up
 rm -rf deb/petit
