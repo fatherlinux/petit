a6ce8382c655fa43be49a41dabd5765451df4a70
smccarty <smccarty@localhost>
2009-08-17 16:47:37 +0000

    * updated deb script to install crunchtools correctly SM

--- petit/build/scripts/deb.sh
+++ petit/build/scripts/deb.sh
@@ -5,6 +5,7 @@ rm -rf deb/petit
 # Create directory structure
 mkdir -p deb/petit/DEBIAN
 mkdir -p deb/petit/usr/bin
+mkdir -p deb/petit/usr/share/petit
 mkdir -p deb/petit/usr/share/doc/petit
 mkdir -p deb/petit/var/lib/petit
 
@@ -21,7 +22,10 @@ cp ../src/COPYING deb/petit/usr/share/doc/petit/COPYING
 cp ../src/README deb/petit/usr/share/doc/petit/README
 
 ## Lib
-rsync -av --exclude=.svn ../src/lib/ deb/petit/var/lib/petit/
+rsync -av --exclude=.svn ../src/lib/fingerprint_library/ deb/petit/var/lib/petit/fingerprint_library/
+rsync -av --exclude=.svn ../src/lib/fingerprints/ deb/petit/var/lib/petit/fingerprints/
+rsync -av --exclude=.svn ../src/lib/filters/ deb/petit/var/lib/petit/filters/
+rsync -av --exclude=.svn ../src/lib/crunchtools.py deb/petit/usr/share/petit/crunchtools.py
 
 # Build the package
 version=`cat deb/control | grep Version | awk '{print $2}'`
