f71544176b98c4c748c182b0c3a08bc6bd6ba2df
root <root@localhost>
2009-09-14 20:56:27 +0000

    * Added basic integration test script SM

---
+++ petit/test/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# 
+# Written By: Scott McCarty <smccarty@eyemg.com>
+# Date: 9/2009
+# Description: Simple integration test to make sure things work rudamentarily well
+
+functions="hash wordcount host daemon sgraph mgraph hgraph"
+
+for function in $functions
+do
+
+	for test in `ls data/*.log`
+	do
+		# Get the right name for the test
+		test=`basename $test | cut -f1 -d"."`
+		echo "Testing Function: $function with $test.log"
+
+		# Run test
+		petit --${function} data/${test}.log > ${test}-${function}.tmp
+
+		if ! diff output/${test}-${function}.output ${test}-${function}.tmp
+		then
+			echo "Fialed $function: Test of: $test"
+			exit 1
+		else
+			echo "Fialed $function: Test of: $test"
+		fi
+
+	done
+done
+
+rm -f ./*.tmp
