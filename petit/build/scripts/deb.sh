bd48a6986e71a3b191ef8cd91ac0090a20af2370
root <root@localhost>
2009-10-01 16:56:46 +0000

    Fixed script for building with new structure SM

--- petit/build/scripts/deb.sh
+++ petit/build/scripts/deb.sh
@@ -3,33 +3,33 @@
 rm -rf deb/petit
 
 # Create directory structure
-mkdir -p deb/petit/DEBIAN
-mkdir -p deb/petit/usr/bin
-mkdir -p deb/petit/usr/share/petit
-mkdir -p deb/petit/usr/share/doc/petit
-mkdir -p deb/petit/var/lib/petit
+mkdir -p build/deb/petit/DEBIAN
+mkdir -p build/deb/petit/usr/bin
+mkdir -p build/deb/petit/usr/share/petit
+mkdir -p build/deb/petit/usr/share/doc/petit
+mkdir -p build/deb/petit/var/lib/petit
 
 # Copy in control file
-cp deb/control deb/petit/DEBIAN/control
+cp build/deb/control build/deb/petit/DEBIAN/control
 
 # Now fill with latest data
 ## Bin
-rsync -av --exclude=.svn ../src/bin/ deb/petit/usr/bin/
+rsync -av --exclude=.svn src/bin/ build/deb/petit/usr/bin/
 
 ## Docs
-cp AUTHORS deb/petit/usr/share/doc/petit/AUTHORS
-cp COPYING deb/petit/usr/share/doc/petit/COPYING
-cp README deb/petit/usr/share/doc/petit/README
+cp AUTHORS build/deb/petit/usr/share/doc/petit/AUTHORS
+cp COPYING build/deb/petit/usr/share/doc/petit/COPYING
+cp README build/deb/petit/usr/share/doc/petit/README
 
 ## Lib
-rsync -av --exclude=.svn ../src/lib/fingerprint_library/ deb/petit/var/lib/petit/fingerprint_library/
-rsync -av --exclude=.svn ../src/lib/fingerprints/ deb/petit/var/lib/petit/fingerprints/
-rsync -av --exclude=.svn ../src/lib/filters/ deb/petit/var/lib/petit/filters/
-rsync -av --exclude=.svn ../src/lib/crunchtools.py deb/petit/usr/share/petit/crunchtools.py
+rsync -av --exclude=.svn src/lib/fingerprint_library/ build/deb/petit/var/lib/petit/fingerprint_library/
+rsync -av --exclude=.svn src/lib/fingerprints/ build/deb/petit/var/lib/petit/fingerprints/
+rsync -av --exclude=.svn src/lib/filters/ build/deb/petit/var/lib/petit/filters/
+rsync -av --exclude=.svn src/lib/crunchtools.py build/deb/petit/usr/share/petit/crunchtools.py
 
 # Build the package
-version=`cat deb/control | grep Version | awk '{print $2}'`
-dpkg -b deb/petit petit_${version}_i386.deb
+version=`cat build/deb/control | grep Version | awk '{print $2}'`
+dpkg -b build/deb/petit petit_${version}_i386.deb
 
 # Clean up
-rm -rf deb/petit
+rm -rf build/deb/petit
