0f28cc32aab0d96cbfd3e61f19e03cf988e9e5f8
Scott McCarty <scott.mccarty@gmail.com>
2011-02-14 22:59:17 -0500

    * Added a lot of introspection to determine log type driver * Offloaded tally logic tests to file type drivers * Automatically iterates over all drivers when performing tally * Basically decoupled driver writing from the rest of class structure * Much easier to add drivers now * Tested build from ipython * Thinking about unit tests :-) * Added small optimization to delete buffer * Thinking about pickling hmmmm Scott M

--- petit/src/lib/crunchtools/CrunchLog.py
+++ petit/src/lib/crunchtools/CrunchLog.py
@@ -1,7 +1,7 @@
 """
-Generic log class which contains a payload of objects which conform to the 
+Generic log class which contains a payload of objects which conform to the
 LogEntry specification.  Log, which is a List (array) of type LogEntry,  is
-relied upon and consumed to build any of the XHash object such as SuperHash
+relied upon and consumed to build any of the XHash objects such as SuperHash
 or GraphHash.
 """
 
@@ -13,69 +13,72 @@ import logging
 from random import choice
 import datetime
 import time
+import types
 
+# Automatically load a list of drivers for each file type this will be used
+# to determine what kind of log it is. Do NOT add the parent LogEntry class
+ma = sys.modules[__name__].__dict__ # module attributes
+entry_types = list()
 
-class Log(UserList):
+for i in ma.keys():
+    if isinstance(ma[i], types.ClassType):
+        if issubclass(ma[i], ma['LogEntry']) and ma[i].__name__ != "LogEntry":
+            entry_types.append(ma[i].__name__)
+
+class CrunchLog(UserList):
+    """
+    Class which extends UserList to provide robust in memory log object
+    """
 
     def __init__(self, filename=""):
         UserList.__init__(self)
 
+        buf = list()
 
-        # Create new array to hold log data
-        buf = self.open_file(filename)
-
-        # Buffer has bow been created and work with file is done
-        # Now it is time to determine what kind of objects will be
-        # used for construction of self.
-
-        # Get the correct subclass
-        Entry = self.select(buf)
-
-        # Finally, build self with the correct subclassed LogEntry constructor type
-        for line in buf:
-            self.append(Entry(line))
-
-    def open_file(self, filename):
-        """Helper function used to open logs"""
-
-        # Create new array to hold log data
-        buf = []
-
-        # Check for empty, logfile, or read stdin
         if filename == "":
             return
         elif filename == "__none__":
-            f = sys.stdin
+            self.f = sys.stdin
         else:
             logging.debug("Opening File: "+filename)   
-            f = open(filename)                         
+            self.f = open(filename)                         
 
-        # Read entire contents into array for speed        
-        for line in f.readlines():                         
-            buf.append(line)                        
+        for line in self.f.xreadlines():
+            buf.append(line)
 
-        # Close file                                       
-        f.close()
+        # Automatically select entry type
+        self.Entry = self.select(buf)
 
-        return buf
+        # Save for introspective purpose
+        self.payload_type = self.Entry.__name__
+        self.file_name = filename
+        self.build_date = datetime.datetime.now()
 
+        # Build from entry type
+        for line in buf:                         
+            self.append(self.Entry(line))
+
+        del buf
+
+    def __del__(self):
+        self.f.close()
 
     def select(self, buf):
-        """Selector which decides what kind of entry to add"""
+        """
+        Determines which type of entry to use when building CrunchLog by 
+        by sampling the buffer and using a quarum based on votes for each
+        log type
+        """
 
         # Setup variables
-        max_sample_lines = 10
+        tally = {}
         sample_lines = []
-        tally = {'SecureLogEntry': 0, \
-                 'SyslogEntry': 0, \
-                 'RSyslogEntry': 0, \
-                 'ApacheAccessEntry': 0, \
-                 'ApacheErrorEntry': 0, \
-                 'SnortEntry': 0, \
-                 'RawEntry': 0}
+        max_sample_lines = 10
         tally_threshold = max_sample_lines/4
 
-        # Check to make sure buffer has data
+        for entry_type in entry_types:
+            tally[entry_type] = 0
+
         if len(buf) >= 1:
 
             # Keep building samples until we get a good set
@@ -87,51 +90,18 @@ class Log(UserList):
 
                 # Build tallies for the collected samples
                 for line in sample_lines:
-                    # Test and select correct entry type
-                    if SecureLogEntry.is_type(line):
-                        tally['SecureLogEntry'] += 1
-                    elif SyslogEntry.is_type(line):
-                        tally['SyslogEntry'] += 1
-                    elif RSyslogEntry.is_type(line):
-                        tally['RSyslogEntry'] += 1
-                    elif ApacheAccessEntry.is_type(line):
-                        tally['ApacheAccessEntry'] += 1
-                    elif ApacheErrorEntry.is_type(line):
-                        tally['ApacheErrorEntry'] += 1
-                    elif SnortEntry.is_type(line):
-                        tally['SnortEntry'] += 1
-                    else:
-                        tally['RawEntry'] += 1
+                    for entry_type in entry_types:
+                        if eval(entry_type).is_type(line):
+                            tally[entry_type] += 1
+                        else:
+                            tally['RawEntry'] += 1
 
-                # Determined which type to return
-                if tally['SecureLogEntry'] >= max_sample_lines:
-                    logging.info("Determined Secure Log: " + \
-                    str(tally['SecureLogEntry']))
-                    return SecureLogEntry
-                elif tally['SyslogEntry'] > tally_threshold:
-                    logging.info("Determined Syslog Log: " + \
-                    str(tally['SyslogEntry']))
-                    return SyslogEntry
-                elif tally['RSyslogEntry'] > tally_threshold:
-                    logging.info("Determined RSyslog Log: " + \
-                    str(tally['RSyslogEntry']))
-                    return RSyslogEntry
-                elif tally['ApacheAccessEntry'] > tally_threshold:
-                    logging.info("Determined Apache Access Log: " + \
-                    str(tally['ApacheAccessEntry']))
-                    return ApacheAccessEntry
-                elif tally['ApacheErrorEntry'] > tally_threshold:
-                    logging.info("Determined Apache Error Log: " + \
-                    str(tally['ApacheErrorEntry']))
-                    return ApacheErrorEntry
-                elif tally['SnortEntry'] > tally_threshold:
-                    logging.info("Determined Snort Log: " + \
-                    str(tally['SnortEntry']))
-                    return SnortEntry
-                elif tally['RawEntry'] > tally_threshold:
-                    logging.info("Determined Raw Log: " + \
-                    str(tally['RawEntry']))
-                    return RawEntry
+
+                # Tally logic is determined by driver
+                for entry_type in entry_types:
+                    if eval(entry_type).tally_logic(tally[entry_type], tally_threshold, max_sample_lines):
+                        logging.info("Determined " + str(entry_type) + ": " + str(tally[entry_type]))
+                        return eval(entry_type)
 
 
     def contains(self, obj):
@@ -140,7 +110,6 @@ class Log(UserList):
             return isinstance(self[len(self)-1], obj)
         else:
             return False
-
 
     def display(self):
         """Simple display function to show entire log"""
@@ -151,7 +120,7 @@ class Log(UserList):
     def subset(self, string):
         """Return a Log object that contains a subset of entries based on a filter"""
 
-        newlog = Log()
+        newlog = CrunchLog()
         for entry in self:
             if re.search(string, entry.log_entry):
                 newlog.append(entry)
@@ -181,6 +150,15 @@ class LogEntry:
               "Second:", self.second, \
               "Host:", self.host, \
               "Payload", self.log_entry
+ 
+    def tally_logic(tally, tally_threshold, max_sample_lines):
+        if tally > tally_threshold:
+            return True
+        else:
+            return False
+
+    # Declare Static Methods
+    tally_logic = staticmethod(tally_logic)
 
     def set_abnormal(self, value):
         self.year, \
@@ -553,6 +531,17 @@ class SecureLogEntry(LogEntry):
     # Declare Static Methods
     is_type = staticmethod(is_type)
 
+    def tally_logic(tally, tally_threshold, max_sample_lines):
+        """Override tally logic for secure logs"""
+
+        if tally >= max_sample_lines:
+            return True
+        else:
+            return False
+
+    # Declare Static Methods
+    tally_logic = staticmethod(tally_logic)
+
 
 class ScriptlogEntry(LogEntry):
     """
@@ -603,12 +592,12 @@ class ScriptlogEntry(LogEntry):
         
 
         # Split the line up
-        value = line.split()
+        value = str(line).split()
 
         if len(value) >= 8:
 
             # Look for special label to determine scriptlog type
-            if re.search(value[5] == label):
+            if re.search(value[5], label):
                 return True
             else:
                 return False
@@ -641,24 +630,20 @@ class RawEntry(LogEntry):
             self.set_blank()
 
     def is_type(line):
-        """Standard function from interface class to determine type"""
+        """
+        Always return False so that raw entry is not selected by mistake
+        """
 
-        if len(line) >= 1:
-
-            # Look for any length of text in the line
-            if re.search(".+",line):
-                return True
-            else:
-                return False
-        else:
-            return False
+        return False
 
     # Declare Static Methods
     is_type = staticmethod(is_type)
 
 
 class SnortEntry(LogEntry):
-    """Driver for Snort formatted log files. Conforms to LogEntry interface class."""
+    """
+    Driver for Snort formatted log files. Conforms to LogEntry interface class.
+    """
 
     def __init__(self, line):
 
@@ -704,7 +689,6 @@ class SnortEntry(LogEntry):
             self.set_blank()
 
     def is_type(line):
-        """Standard function from interface class to determine type"""
 
         global logging
 
